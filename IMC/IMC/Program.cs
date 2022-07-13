f6592d47fa4dc6de4bf35d0f4f978bf7a0089d62
Fabio <akki.fabio@gmail.com>
2022-07-13 17:06:16 -0300

    Enviando atividade IMC

---
+++ IMC/IMC/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com peso");
+            float peso = (float) Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Entre com altura");
+            float altura = (float)Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Peso da pessoa:  " + peso);
+            Console.WriteLine($"Altura da pessoa:  {altura}");
+
+            float result = peso / (altura * altura);
+
+            if (result < 18)
+            {
+                Console.WriteLine("Abaixo do Peso");
+            }
+                else if (result >= 18 && result < 25)
+                {
+                Console.WriteLine("Peso Normal");
+                }
+                    else if (result >=25 && result < 30)
+                    {
+                    Console.WriteLine("Sobrepeso");
+                    }
+                        else if (result >= 30 && result < 40)
+                        {
+                        Console.WriteLine("Obesidade");
+                        }
+                            else
+                            {
+                            Console.WriteLine("Obesidade Grave");
+                            }
+        }
+    }
+}
