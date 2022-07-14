3d5d33502d44296236330a3b1053c897c0b718f9
Fabio <akki.fabio@gmail.com>
2022-07-14 12:05:10 -0300

    Completando o exercício de IMC

--- IMC/IMC/Program.cs
+++ IMC/IMC/Program.cs
@@ -6,23 +6,36 @@ namespace IMC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Entre com peso");
+            Console.WriteLine("Entre com nome");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Entre com idade (em número inteiro)");
+            float idade = (float)Convert.ToDouble(Console.ReadLine());
+            //Para ler idade como texto (string):   
+            //Console.WriteLine("Entre com idade");
+            //string idade2 = Console.ReadLine();
+
+            Console.WriteLine("Entre com peso (use vírgula se necessário)");
             float peso = (float) Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Entre com altura");
+            Console.WriteLine("Entre com altura (use vírgula)");
             float altura = (float)Convert.ToDouble(Console.ReadLine());
 
-
+            Console.WriteLine("Nome da pessoa:  " + nome);
+            Console.WriteLine("Idade da pessoa:  " + idade);
+            //Console.WriteLine("Idade da pessoa:  " + idade2);
             Console.WriteLine("Peso da pessoa:  " + peso);
             Console.WriteLine($"Altura da pessoa:  {altura}");
 
             float result = peso / (altura * altura);
+            // caso queira usar fórmula
+            //float result = peso / Math.Pow(altura,2)
 
-            if (result < 18)
+            if (result < 18.5)
             {
                 Console.WriteLine("Abaixo do Peso");
             }
-                else if (result >= 18 && result < 25)
+                else if (result >= 18.5 && result < 25)
                 {
                 Console.WriteLine("Peso Normal");
                 }
@@ -38,6 +51,8 @@ namespace IMC
                             {
                             Console.WriteLine("Obesidade Grave");
                             }
+            Console.WriteLine("Valor do IMC da pessoa:  " + Math.Round(result, 2));
+            //Console.WriteLine($"Valor do IMC da pessoa:  {Math.Round(result, 2)}"); //Utilizando $
         }
     }
 }
